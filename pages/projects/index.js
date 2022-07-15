7760c58f309e481c411c85577be68f352a94fce4
dhruv <dhruvbakshi12343@gmail.com>
2022-07-16 03:36:46 +0530

    Added pics in project page

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -13,14 +13,13 @@ const Projects = () => {
                 Projects
             </h2>
             <LayoutWidth>
-                <ProjectCard heading={"01"} subheading={"swarming tech"} info={"Lorem Ipsum is simply dummy text of the printing and typesetting industry."} image="/assets/project-banner.jpg"/>
-                <ProjectCard heading={"02"} subheading={"swarming tech"} info={"Lorem Ipsum is simply dummy text of the printing and typesetting industry."} image="/assets/project-banner.jpg" right={true}/>
+                <ProjectCard heading={"01"} subheading={"Nano Drones"} image="/assets/projects/nano.jpg"  imageCss="h-[350px] object-cover"/>
+                <ProjectCard heading={"02"} subheading={"Hydrostatic Drones"} image="/assets/projects/hydro.jpg" imageCss="h-[350px] object-cover" right={true}/>
                 
-                <ProjectCard heading={"03"} subheading={"swarming tech"} info={"Lorem Ipsum is simply dummy text of the printing and typesetting industry."} image="/assets/project-banner.jpg"/>
-                <ProjectCard heading={"04"} subheading={"swarming tech"} info={"Lorem Ipsum is simply dummy text of the printing and typesetting industry."} image="/assets/project-banner.jpg" right={true}/>
+                <ProjectCard heading={"03"} subheading={"Drone Light Shows"} image="/assets/projects/light.png" imageCss="h-[350px] object-cover"/>
+                <ProjectCard heading={"04"} subheading={"Inspection Drones"} image="/assets/projects/inspection.jpg" right={true} imageCss="h-[350px] object-cover"/>
                 
-                <ProjectCard heading={"05"} subheading={"swarming tech"} info={"Lorem Ipsum is simply dummy text of the printing and typesetting industry."} image="/assets/project-banner.jpg"/>
-                <ProjectCard heading={"06"} subheading={"swarming tech"} info={"Lorem Ipsum is simply dummy text of the printing and typesetting industry."} image="/assets/project-banner.jpg" right={true}/>
+                <ProjectCard heading={"05"} subheading={"Surveillance Drones"} image="/assets/projects/surveillance.jpg" imageCss="h-[350px] object-cover"/>
                 
             </LayoutWidth>
             <Footer />
